50a13d788ac5837af2a98ea2c63bdfe99486c172
Diogo Freire <didi99@live.com.pt>
2021-01-16 19:44:14 +0000

    Teste

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame
-from inputs import input
 
 def main():
     pygame.init()
 
-    key = pygame.key.get_pressed()
-
-    res = (1024, 768)
+    screenX = 1024
+    screenY = 768
+    res = (screenX, screenY)
 
     screen = pygame.display.set_mode(res)
 
@@ -18,15 +17,42 @@ def main():
     while (True):
         #Busca o evento
         events = pygame.event.get()
+        key = pygame.key.get_pressed()
 
+        #Quando é carregada a tecla W
+        if key[pygame.K_w]:
+            pos = (pos[0], pos[1] - 1)
+            if pos[1] < 0:
+                pos = (pos[0], 0)
+
+        #Quando é carregada a tecla S
+        if key[pygame.K_s]:
+            pos = (pos[0], pos[1] + 1)
+            if pos[1] > screenY:
+                pos = (pos[0], screenY)
+
+        #Quando é carregada a tecla A
+        if key[pygame.K_a]:
+            pos = (pos[0] - 1, pos[1])
+            if pos[0] < 0:
+                pos = (0, pos[1])
+
+        #Quando é carregada a tecla D
+        if key[pygame.K_d]:
+            pos = (pos[0] + 1, pos[1])
+            if pos[1] > screenX:
+                pos = (pos[0], screenX)
+        
+            #Quando é carregada a tecla ESC
         for event in events:
-            if (event.type == pygame.QUIT):
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+                    
+            #Quando é carregado o botão para sair do programa
+            if event.type == pygame.QUIT:
                 exit()
 
-        input(pos, res, key)
-        
-        if (key[pygame.K_ESCAPE]):
-            break
 
         # Desenha
         screen.fill(screen_color)
